09380e8814e75684147cabfaa65da97122aaf5d6
NIGHTLePoulet <197069077+NIGHTLePoulet@users.noreply.github.com>
2026-08-27 12:11:48 +0200

    feat: add user post relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Models;
-
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -28,5 +28,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts(): hasMany
+    {
+        return $this->hasMany(Post::class);
     }
 }
