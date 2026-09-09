95a5781a272b11b39d80ea41dd2b62829b3b9b4e
NIGHTLePoulet <197069077+NIGHTLePoulet@users.noreply.github.com>
2026-09-09 11:59:48 +0200

    test: add log in feature test

---
+++ tests/Browser/Auth/LoginTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+
+it('user can log in', function () {
+    $user = User::factory()->create([
+        'username' => 'JohnDoe',
+        'email' => 'johndoe@example.com',
+        'password'=> 'password',
+    ]);
+
+    $page = visit('/login');
+
+    $page
+        ->fill('email', 'johndoe@example.com')
+        ->fill('password', 'password')
+        ->submit();
+
+    $this->assertAuthenticated();
+
+    $page->assertPathis('/');
+});
